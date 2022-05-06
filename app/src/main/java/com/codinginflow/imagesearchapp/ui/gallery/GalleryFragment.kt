cb5c82ec5f6d5f35f0d83ba44960414d35dd7034
unknown <anthonyangatia@gmail.com>
2022-05-06 11:28:34 +0300

    Part 10 load state listener

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -5,8 +5,10 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.codinginflow.imagesearchapp.R
 import com.codinginflow.imagesearchapp.databinding.FragementGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,13 +28,36 @@ class GalleryFragment : Fragment(R.layout.fragement_gallery) {
         val adapter = UnsplashPhotoAdapter()
         binding.apply {
             recyclerView.setHasFixedSize(true)
+            recyclerView.itemAnimator = null
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
                 footer = UnsplashPhotoLoadStateAdapter { adapter.retry() }
             )
+            buttonRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
         viewModel.photo.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+        adapter.addLoadStateListener { loadState->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+                //EMPTY VIEW
+                if(loadState.source.refresh is LoadState.NotLoading &&
+                        loadState.append.endOfPaginationReached &&
+                        adapter.itemCount<1){
+                    recyclerView.isVisible  = false
+                    textViewEmpty.isVisible = true
+                }else{
+                    textViewEmpty.isVisible = false
+                }
+            }
+
         }
         setHasOptionsMenu(true)
 
